cc5440045aa9ba413c232b0dd912e0421a0eb9aa
bhaskar <bhaskarjha95@gmail.com>
2025-05-02 16:21:33 +0530

    renamed app.js to index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+// index.js
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, DevOps!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
